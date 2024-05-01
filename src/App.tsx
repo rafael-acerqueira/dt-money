2a3771f0fdda77f84ad3ef377efa4b34b85f9b32
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-05-01 10:32:20 -0300

    Add Summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
+import { Transactions } from "./pages/Transactions"
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <p>
-        Initial Setup
-      </p>
+      <Transactions />
     </ThemeProvider>
   )
 }
